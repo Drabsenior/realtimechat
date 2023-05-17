a14931673c03572a801d4fda4279e6d7c2bfbd10
Drabsenior <drabsenior@gmail.com>
2023-05-17 15:23:00 +0300

    initial nextaut setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  await db.set("name", "abbenezer");
   return (
     <div>
       <Button size="lg" variant="ghost">
-        hello
+        {process.env.UPSTASH_REDIS_REST_URL}
       </Button>
     </div>
   );
